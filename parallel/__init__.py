43677cebbae76125e632d20f9b4f6b0971dcc040
lichong0309 <chongli0309@gmail.com>
2022-09-01 18:15:01 +0800

    update

--- parallel/__init__.py
+++
@@ -1,2 +0,0 @@
-from .graphparallel import *
-from .dataloader import *
--- partition/__init__.py
+++
@@ -1,3 +0,0 @@
-from .partition import kl_2partition
-from .refine import build_train_graph
-from .refine import wrap_neighbor
